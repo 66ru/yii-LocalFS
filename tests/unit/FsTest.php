d00d5e3081fa81f50365bc14480117244a6d1b6b
bazilio <bazilio91@gmail.com>
2012-10-15 19:00:26 +0600

    seg fault debug

--- tests/unit/FsTest.php
+++ tests/unit/FsTest.php
@@ -1,5 +1,5 @@
 <?php
-//require_once "ETestCase.php";
+require_once "ETestCase.php";
 class FsTest extends CTestCase
 {
 	private $fixturesPath;
@@ -18,11 +18,11 @@ class FsTest extends CTestCase
 		/**
 		 * @var BaseFile $file
 		 */
-		/*$file = Yii::app()->fs->publishFile($this->getFixturesPath() . 'BaseFile.txt');
+		$file = Yii::app()->fs->publishFile($this->getFixturesPath() . 'BaseFile.txt');
 		$this->assertEquals(get_class($file), 'BaseFile', 'matching file class');
 		$fileUrl = $file->getUrl();
 		$fileContent = @file_get_contents($fileUrl);
-		$this->assertEquals($fileContent, 'good', 'load file from url');*/
+		$this->assertEquals($fileContent, 'good', 'load file from url');
 	}
 
 	public function testPublishImageFile()
@@ -30,18 +30,18 @@ class FsTest extends CTestCase
 		/**
 		 * @var ImageFile $file
 		 */
-		/*$file = Yii::app()->fs->publishFile($this->getFixturesPath() . 'ImageFile.jpg');
+		$file = Yii::app()->fs->publishFile($this->getFixturesPath() . 'ImageFile.jpg');
 		$this->assertEquals(get_class($file), 'ImageFile', 'Matching file class');
 		$fileUrl = $file->getUrl();
 		$this->assertUrlExists($fileUrl,'Check ImageFile by url');
-		$this->assertFileExists($file->getPath());*/
+		$this->assertFileExists($file->getPath());
 	}
 
 	public function testThumbnail()
 	{
 
 		/** @var $file ImageFile */
-		/*$file = Yii::app()->fs->publishFile($this->getFixturesPath() . 'ImageFile.jpg');
+		$file = Yii::app()->fs->publishFile($this->getFixturesPath() . 'ImageFile.jpg');
 		$url = $file->getThumbnail(array(200,200));
 		$this->assertUrlExists($url,'Check 200x200 thumb by url');
 
@@ -58,6 +58,6 @@ class FsTest extends CTestCase
 		$this->assertUrlExists($url,'Check 400x200 cropZoom thumb by url');
 
 		$url = $file->getThumbnail(array(200,400, 'cz' => true));
-		$this->assertUrlExists($url,'Check 200x400 cropZoom thumb by url');*/
+		$this->assertUrlExists($url,'Check 200x400 cropZoom thumb by url');
 	}
 }
